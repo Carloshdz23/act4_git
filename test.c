2c45a1356f39580b8d0b7c4c757a56e3fbef97be
Carlos Hernández <cdavid.hernandez@academicos.udg.mx>
2020-09-21 21:20:32 -0500

    Iniciamos un menú funcional con do-while

---
+++ test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    char opc;
+
+    do
+    {
+        printf("1. Capturar Enteros\n");
+        printf("2. Mostrar 'n' veces la Cadena\n");
+        printf("3. Agrega Personaje\n");
+        printf("4. Salir\n");
+        scanf("%c",&opc);
+
+        switch(opc)
+        {
+        case '1':
+
+            break;
+        case '2':
+
+            break;
+        case '3':
+
+            break;
+        default:
+            break;
+        }
+    } while(opc!='4');
+        
+    return 0;
+}
